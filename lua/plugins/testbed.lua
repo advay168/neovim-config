87433dacb319e5e9f025e7e2148d9717ff0bdd70
Advay <advay168@gmail.com>
2023-10-13 21:04:48 +0100

    Added markdown-preview and vim-tex

--- lua/plugins/testbed.lua
+++ lua/plugins/testbed.lua
@@ -1,6 +1,35 @@
 return {
   {
+    "iamcco/markdown-preview.nvim",
+    build = ":call mkdp#util#install()",
+    config = function()
+      vim.g.mkdp_auto_close = 0
+      vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>")
+      local group = vim.api.nvim_create_augroup("loadNotes", {})
+      vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+        group = group,
+        pattern = { "*/notes/**/*.md" },
+        command = "MarkdownPreview",
+      })
+      --[[ vim.api.nvim_create_autocmd({ "BufWipeout" }, {
+        group = group,
+        pattern = { "*/notes/**/*.md" },
+        callback = function()
+          vim.cmd "call mkdp#rpc#preview_close()"
+        end
+      }) ]]
+    end
+  },
+  {
+    "lervag/vimtex",
+    config = function()
+      vim.g.vimtex_compiler_progname = 'nvr'
+      vim.g.vimtex_view_method = 'sioyek'
+    end
+  },
+  {
     "windwp/nvim-autopairs",
+    enabled = false,
     event = "InsertEnter",
     config = function()
       require('nvim-autopairs').setup({
